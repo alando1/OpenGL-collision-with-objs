8a404716c14ea54a09cfbd1f1a2a36470b2d330f
Alan Rodriguez <arodriguez7@mail.usf.edu>
2017-02-19 20:12:43 -0500

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
-g++ -std=c++11 Portal.cpp Terrain.cpp suppliedGlutFuncs.cpp DrawFunc.cpp 256main.cpp -o run -O3 -I"./include" -L"./lib"  -lSDL2 -lSDL2main -lSDL2_mixer -lfreeglut -lSOIL -lopengl32 -lglu32 
+g++ -std=c++11 Bullet.cpp Portal.cpp Terrain.cpp suppliedGlutFuncs.cpp DrawFunc.cpp 256main.cpp -o run -O3 -I"./include" -L"./lib"  -lSDL2 -lSDL2main -lSDL2_mixer -lfreeglut -lSOIL -lopengl32 -lglu32 
 
 @echo off
 echo(
