c06d77dfdabfdc620d0a542362d14c30cc278a73
Alan Rodriguez <arodriguez7@mail.usf.edu>
2017-02-28 22:28:53 -0500

    Add files via upload

---
+++ Material.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <cstdlib>
+#include <string>
+#include <iostream>
+#include <cmath>
+#include <chrono>
+#include <fstream>
+#include <vector>
+#include "Vec3.h"
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+#include <SOIL.h>
+
+using namespace std;
+
+class Material
+{
+
+};
